44376c18da7592f9e9d0cdc1444306f34e25481a
Armad1ll0 <67917670+Armad1ll0@users.noreply.github.com>
2022-03-11 17:52:11 +0000

    Update leapfrog.py
    mistakes in NLP grad assignment

--- HMC_functions.py/leapfrog.py
+++ HMC_functions.py/leapfrog.py
@@ -28,13 +28,13 @@ def leapfrog(step_size, trajectory_length, p, x, NLP_grad, inv_cov, M):
     be used for extra samples when trying to estimate the covariance or mass matrix
     '''
     
-    p = p - 0.5*step_size*(M @ neg_log_prob_grad(x, A))
+    p = p - 0.5*step_size*(M @ NLP_grad(x, A))
     in_between = []
     for i in range(int(trajectory_length) - 1):
         x = x + step_size*(p @ np.linalg.inv(M))
         in_between.append(x)
-        p = p - 0.5*step_size*(M @ neg_log_prob_grad(x, A))
+        p = p - 0.5*step_size*(M @ NLP_grad(x, A))
     x = x + step_size*(p @ np.linalg.inv(M))
-    p = p - 0.5*step_size*(M @ neg_log_prob_grad(x, A))
+    p = p - 0.5*step_size*(M @ NLP_grad(x, A))
     
     return x, p, in_between 
