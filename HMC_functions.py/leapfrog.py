c422f0e135225409797d99fdfa6a8c7afe613594
Armad1ll0 <67917670+Armad1ll0@users.noreply.github.com>
2022-03-11 17:10:53 +0000

    Update leapfrog.py
    adding in the comments for inputs and outputs

--- HMC_functions.py/leapfrog.py
+++ HMC_functions.py/leapfrog.py
@@ -1,5 +1,33 @@
 #Basic leapfrog algorithm 
+import numpy as np 
+
 def leapfrog(step_size, trajectory_length, p, x, NLP_grad, inv_cov, M):
+    '''
+    inputs: 
+    trajector_length is how long we want our hamiltonian dynamics to run for 
+    
+    step_size is the size of the step we want to do in between each trajectory length, 
+    e.g. if we have a trajectory length of 1 and a step size of 0.1, we will have 10 steps 
+    to do out full trajectory length 
+    
+    p is the gaussian picked momentum 
+    
+    x is our current position
+    
+    NLP_grad is the gradient of the negative log probability 
+    
+    inv_cov is the inverse of our covariance 
+    
+    M is the mass matrix, if not doing an adaptive mass matrix part then this is just an 
+    identity matrix 
+    
+    outputs:
+    x, p are the new momentums and positions calculated via the leapfrog algorithm 
+    
+    in_between is the positions in between each leapfrog step, we take these as they can 
+    be used for extra samples when trying to estimate the covariance or mass matrix
+    '''
+    
     p = p - 0.5*step_size*(M @ neg_log_prob_grad(x, A))
     in_between = []
     for i in range(int(trajectory_length) - 1):
@@ -8,4 +36,5 @@ def leapfrog(step_size, trajectory_length, p, x, NLP_grad, inv_cov, M):
         p = p - 0.5*step_size*(M @ neg_log_prob_grad(x, A))
     x = x + step_size*(p @ np.linalg.inv(M))
     p = p - 0.5*step_size*(M @ neg_log_prob_grad(x, A))
+    
     return x, p, in_between 
