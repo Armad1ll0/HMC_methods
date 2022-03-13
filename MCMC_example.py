08227d39dcca6b75fef5ae0ecc996a67019bb574
Armad1ll0 <67917670+Armad1ll0@users.noreply.github.com>
2022-03-13 12:16:51 +0000

    Create MCMC_example.py
    example MCMC script

---
+++ MCMC_example.py
@@ -0,0 +1,29 @@
+import MCMC_chain as MCMC_chain 
+from MCMC_subfunctions import proposed, p_acc_MH, sample_MH
+from sklearn.datasets import make_spd_matrix
+import numpy as np 
+import matplotlib.pyplot as plt 
+
+dimensions = 2
+init = np.zeros(dimensions)
+inv_cov = make_spd_matrix(dimensions)
+step_size = 1
+n_total = 1000
+
+def gaussian(x, inv_cov):
+    xT = x.T
+    first = np.dot(xT, inv_cov)
+    return np.exp(-0.5*np.dot(first, x))
+
+def log_prob(x, inv_cov):
+    xT = x.T
+    first = np.dot(xT, inv_cov)
+    return -0.5*np.dot(first, x)
+  
+MCMC_chain, MCMC_acceptance_rate = MCMC_chain(init, step_size, n_total, log_prob)
+MCMC_results = np.asarray(MCMC_chain)
+
+#plot the scatter results
+plt.scatter(MCMC_results.T[0, :], MCMC_results.T[1, :], color='b', s=0.1)
+plt.title('MCMC Samples')
+plt.show()
