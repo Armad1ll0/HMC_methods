c467fafdc8227f65f637c864b91f1fe9ee1fa213
Armad1ll0 <67917670+Armad1ll0@users.noreply.github.com>
2022-03-13 10:54:24 +0000

    Create MCMC_chain.py
    https://www.tweag.io/blog/2020-08-06-mcmc-intro3/ a lot of this code has been taken from this article, HMC code was based on this as well.

---
+++ MCMC_functions/MCMC_chain.py
@@ -0,0 +1,27 @@
+
+def MCMC_chain(init, step_size, n_total, log_prob):
+    '''
+    inputs:
+    init is the initial starting point for the sampler 
+    
+    step_size is how far we are allowed to sample away from our current sample 
+    
+    n_total is the number of total samples we want to return 
+    
+    log_prob is the log probability of the pdf we are sampling from 
+    
+    outputs:
+    chain is the chain of sample after the algorithm has run for n_total iterations 
+    
+    acceptance rate is the amount of new samples that are accepted
+    
+    '''
+    n_accepted = 0
+    chain = [init]
+    for i in range(n_total):
+        accept, state = sample_MH(chain[-1], log_prob, step_size)
+        chain.append(state)
+        n_accepted += accept
+    acceptance_rate = n_accepted/float(n_total)
+    
+    return chain, acceptance_rate 
